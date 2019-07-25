eb7af6502de1320d3940bd4d3e1405d307b39b38
putra <putrakips@gmail.com>
2019-07-25 15:29:29 +0700

    memperbaiki templates

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,56 +1,62 @@
+<!-- Begin Page Content -->
 <div class="container-fluid">
-    <div class="bd-example">
-        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="<?= base_url('assets/img/slider1.jpg'); ?>" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="<?= base_url('assets/img/slider2.jpg'); ?>" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="<?= base_url('assets/img/slider1.jpg'); ?>" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </div>
-                </div>
+
+    <!-- Page Heading -->
+    <!-- <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    </div> -->
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="<?= base_url('assets/img/slider1.jpg'); ?>" alt="First slide">
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="<?= base_url('assets/img/slider2.jpg'); ?>" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="<?= base_url('assets/img/slider1.jpg'); ?>" alt="Third slide">
+            </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
+    <!-- Content Row -->
     <div class="row text-center mt-3">
+        <!-- Pending Requests Card Example -->
         <?php foreach ($barang as $brg) : ?>
-            <div class="card ml-2 mb-3" style="width: 20rem;">
-                <img src="<?php echo  base_url() . '/uploads/' . $brg->gambar ?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $brg->nama_brg ?></h5>
-                    <small><?= $brg->keterangan ?></small><br>
-                    <span class="badge badge-success mb-3">Rp. <?= $brg->harga ?></span><br>
-                    <a href="#" class="btn btn-primary btn-sm">Tambah Ke Keranjang</a>
-                    <a href="#" class="btn btn-success btn-sm">Detail</a>
+            <div class="col-xl-3 col-sm-12 col-md-6 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="<?= base_url() . '/uploads/' . $brg->gambar; ?>" alt="Card image cap">
+                    <div class="card-body">
+                        <h6 class="card-title"><?= $brg->nama_brg ?></h6>
+                        <small><?= $brg->keterangan ?></small><br>
+                        <span class="badge badge-pill badge-success mb-3">Rp. <?= $brg->harga ?></span><br>
+                        <a href="" class="btn btn-sm btn-primary "> Tambah ke Keranjang</a>
+                        <a href="" class="btn btn-sm btn-success "> Detail</a>
+                    </div>
                 </div>
             </div>
-        <?php endforeach; ?>
+        <?php endforeach ?>
     </div>
+
+
+
+
 </div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
